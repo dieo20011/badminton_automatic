c522cba5b20371a21b3917bada31bc6331a78956
Luan Duy Pham <luan.pham@tscmiami.com>
2026-03-02 16:14:55 +0700

    Update deloy

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    DOMAIN: 'https://todolist-angular-tau.vercel.app/', // Update with your actual Vercel domain
+    DOMAIN: 'https://badmintonauto.vercel.app', // Update with your actual Vercel domain
     API_DOMAIN: 'https://be-todolist.onrender.com', // Update with your actual Render API domain
     API_NEW_KEY: 'b810a73fabed4ec187bff9e8165571cd',
 }; 
